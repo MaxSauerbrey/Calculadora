ce589e6ff704ab6d9a3a2be68be9b5f3ce328a04
dalma-ponce <p.d.f.1915@gmail.com>
2022-11-28 13:50:53 -0300

    agregué la funcion resto

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,10 @@ public class Main {
         System.out.println(num1 * num2);
     }
 
+    public static void resto(int num1, int num2) {
+        System.out.println(num1 % num2);
+    }
+
 
 
 }
