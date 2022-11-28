19f74c0d113d4949d24a83efb2b19262016cc1f1
Max <maxsauerbrey@gmail.com>
2022-11-28 12:58:27 -0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
     }
+    public static void suma (int num1, int num2){
+        System.out.println(num1+num2);
+    }
+
+
+
+
 }
