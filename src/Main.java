100a675cbc29cccfd54315008dc94c64298bd610
FerGerez <fernando.gerez30@gmail.com>
2022-11-29 10:28:00 -0300

    Se agregó la función validador de ceros

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,16 @@ public class Main {
         System.out.println(num1 * num2);
     }
 
+    public static void restringirCeros(int num1,int num2) {
+
+        if (num2 != 0) {
+            System.out.println(num1/num2);
+        } else {
+            System.out.println("No se puede dividir por cero")
+        }
+
+    }
+
 
 
 }
